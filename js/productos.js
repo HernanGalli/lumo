b7f797cc7f5cf300ead930a46c5338634f1b8b5e
HernanGalli <126527229+HernanGalli@users.noreply.github.com>
2025-09-21 15:26:01 -0300

    Create productos.js

---
+++ js/productos.js
@@ -0,0 +1,56 @@
+// js/productos.js
+
+const catalogoLUMO = [
+    {
+      imagen: "imagenes/lampara-dahlia-amarilla.jpg",
+      nombre: "Lámpara Dahlia",
+      categoria: "lamparas",
+      descripcion: "Diseño orgánico y moderno que crea una atmósfera cálida y acogedora en cualquier espacio. Diseño escultural de 25 cm.",
+      oferta: false,
+      //oferta: true,
+      // --- AJUSTE AQUÍ ---
+      precioOriginal: 1190.00, // El precio anterior
+      //precioOferta: 99.00      // El nuevo precio de oferta
+    },
+    {
+      imagen: "imagenes/lampara-dahlia-naranja.jpg",
+      nombre: "Lámpara Dahlia (Naranja)",
+      categoria: "lamparas",
+      descripcion: "La lámpara Dahlia en un vibrante color naranja para un look más atrevido. Diseño escultural de 25 cm.",
+      oferta: false,
+      precioOriginal: 1190.00
+    },
+    {
+      imagen: "imagenes/percheros-cactus.jpg",
+      nombre: "Percheros Cactus",
+      categoria: "hogar",
+      descripcion: "Set de 3 percheros decorativos y funcionales. Ideales para dar un toque divertido a tus paredes.",
+      oferta: false,
+      precioOriginal: 990.00 // Los productos sin oferta solo necesitan el precioOriginal
+    },
+    {
+      imagen: "imagenes/moldes-galletas.jpg",
+      nombre: "Moldes para Galletas",
+      categoria: "pasteleria",
+      descripcion: "Set de moldes con formas personalizadas. Fabricados con material de grado alimenticio.",
+      oferta: false,
+      precioOriginal: 210.00
+    },
+
+    {
+        imagen: "imagenes/lampara-dahlia-gris.jpg",
+        nombre: "Lámpara Dahlia (Gris)",
+        categoria: "lamparas",
+        descripcion: "Versión en gris elegante y minimalista, perfecta para ambientes sobrios y contemporáneos. Diseño escultural de 25 cm.",
+        oferta: false,
+        precioOriginal: 1190.00
+    },
+    {
+        imagen: "imagenes/lampara-aloe-blanco.jpg",
+        nombre: "Lámpara Aloe (Balnco y Lila)",
+        categoria: "lamparas",
+        descripcion: "Su textura vertical y base de color vibrante la convierten en una pieza de arte única. Diseño escultural de 25 cm.",
+        oferta: false,
+        precioOriginal: 1190.00
+    }
+];
